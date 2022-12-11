d9cbaaa6e2aa670b51278871e65cbb854be1852c
Lola Khudoyberdieva <lola.kh1994@gmail.com>
2022-12-11 20:33:32 +0300

    add trending section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import showsData from './data.json'
 import ShowItem from './components/ShowItem'
 
 function App() {
-  console.log('data', showsData)
+  console.log(showsData)
+  const trendingShows = showsData.filter((show) => show.isTrending)
+  const generalShows = showsData.filter((show) => !show.isTrending)
+  const bookmarkedShows = showsData.filter((show) => show.isBookmarked)
+  const movies = showsData.filter((show) => show.category === 'Movie')
+  const tvSeries = showsData.filter((show) => show.category === 'TV Series')
+
   return (
     <div className="App">
+      <section className="trending-section">
+        <div className="trending-scroll-section">
+          {trendingShows.map((show) => (
+            <ShowItem key={show.title} show={show} isTrendingPage={true} />
+          ))}
+        </div>
+      </section>
       <section className="general-section">
-        {showsData.map((show) => (
+        {generalShows.map((show) => (
           <ShowItem key={show.title} show={show} isTrendingPage={false} />
         ))}
       </section>
